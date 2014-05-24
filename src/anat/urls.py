8e5cf7d0027bb9a5251b22b52e2e71815682511e
Martin Green <martin.speleo@gmail.com>
2014-05-24 12:42:33 +0100

    Redirect home page to images

--- src/anat/urls.py
+++ src/anat/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import RedirectView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,7 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'anat.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', RedirectView.as_view(url='/images', permanent=False)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^images/', include('dicomImage.urls')),
 )
